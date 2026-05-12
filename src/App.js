76be6d87755f162daf4790fbbb2ce54060f58393
JeffersonXd <196490824+JeffersonCMP1@users.noreply.github.com>
2026-05-12 13:27:52 -0500

    anadi botones

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import freecodecampLogo from './imagenes/freecodecamp-logo.png';
+import Boton from './componentes/Boton';
 
 function App() {
+
+  const manejarClic=()=>{
+    console.log('clic');
+  }
+
+  const reiniciarContador=()=>{
+    console.log('Reiniciar');
+  }
   return (
-    <div className="App">
-      <h1>CAMBIOS EN DOCKER , GITHUB Y EN MI LOCAL</h1>
+    <div className='App'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img className='freecodecamp-logo' src={freecodecampLogo} alt='logo de freecodecamp'/>
+      </div>
+
+      <div className='contenedor-principal'>
+        <Boton 
+        texto='Clic' 
+        esBotonDeClic={true} 
+        manejarClic={manejarClic}>
+        </Boton>
+
+        <Boton 
+        texto='Reiniciar' 
+        esBotonDeClic={false}
+         manejarClic={reiniciarContador}>
+         </Boton>
+      </div>
     </div>
   );
 }
---
+++ src/componentes/Boton.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../hojas-de-estilo/Boton.css' ;
+// function Boton(props){
+//   return(
+//     <button>
+//       {props.texto}
+//     </button>
+//   )
+// }
+
+function Boton({texto,esBotonDeClic,manejarClic}){
+  return(
+    <button className={esBotonDeClic?'boton-clic':'boton-reiniciar'} onClick={manejarClic}>
+      {texto}
+    </button>
+  )
+}
+
+export default Boton;
